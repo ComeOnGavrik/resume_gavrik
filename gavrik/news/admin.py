a0c05802ac1218494a82f79ea9ed833a66af8d75
ComeOnGavrik <mihailgavrik03@gmail.com>
2024-09-26 16:34:25 +0300

    Автоматом подгурежается время и дата на сайт с учетом минского времени, В админке теперь показывается Время первоначального доабавления

--- gavrik/news/admin.py
+++ gavrik/news/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Articles
+#
+# admin.site.register(Articles)
 
-admin.site.register(Articles)
+@admin.register(Articles)
+class ArticlesAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
