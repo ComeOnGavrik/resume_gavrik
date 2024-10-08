7ab22d8ee0a7f802ab8debd940c9c0f603a5784e
ComeOnGavrik <mihailgavrik03@gmail.com>
2024-10-08 20:37:50 +0300

    Не доделал

--- gavrik/shop/views.py
+++ gavrik/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from products.models import ProductImage
+
+
 # Create your views here.
 def shop_home(request):
-    products = ProductImage.objects.filter(is_active=True, is_main=True)
+    product = ProductImage.objects.filter(is_active=True, is_main=True)
     return render(request, 'shop/shop_home.html', locals())
