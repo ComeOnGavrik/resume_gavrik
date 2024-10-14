00f980f7a37a20015db4a3f06c67a4785d7cd3f5
ComeOnGavrik <mihailgavrik03@gmail.com>
2024-10-14 14:45:56 +0300

    полная шляпа

--- gavrik/products/urls.py
+++ gavrik/products/urls.py
@@ -1,6 +1,6 @@
-# from . import views
-# from django.urls import path, include
-#
+from . import views
+from django.urls import path, include
+
 # urlpatterns = [
 #     path('<int:pk>', views.ProductDetailView.as_view(), name='product_detail')
 # ]
--- gavrik/shop/urls.py
+++ gavrik/shop/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path, include
+from products.views import ProductDetailView
 
 urlpatterns = [
-    path('home', views.shop_home, name='shop_home')
+    path('home', views.shop_home, name='shop_home'),
+    path('<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
