5f25ba429a8383183d50fde4ce6a0cd129e8731d
ComeOnGavrik <mihailgavrik03@gmail.com>
2024-11-08 20:09:42 +0300

    Начал делать 1.8.1 Тайм код 14:35 22 минута 1.8.1

--- gavrik/static/shop/js/scripts.js
+++ gavrik/static/shop/js/scripts.js
@@ -3,10 +3,21 @@ $(document).ready(function(){
     console.log(form);
     form.on('submit', function(e){
         e.preventDefault();
-        console.log('123');
-
+        console.log('Test');
         var nmb = $('#number').val();
-
         console.log(nmb);
+        var submit_btn = $('#submit_btn');
+        var product_id = submit_btn.data("product_id");
+        var product_name = submit_btn.data("product_name");
+        var product_price = submit_btn.data("product_price")
+        console.log(product_id);
+        console.log(product_name);
+        console.log(product_price);
     })
+//
+//    $('.basket-container').on('click', 'hover', function(e){
+//        e.preventDefault();
+//        $('.basket-item').removeClass('hidden')
+//    }
+//    }
 });
