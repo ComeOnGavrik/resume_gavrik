e87e9467c3fb4d9e1b88325a7ec26ee35bef646b
ComeOnGavrik <mihailgavrik03@gmail.com>
2024-11-09 15:21:22 +0300

    Начал делать 1.8.1 Тайм код 14:35 22 минута 1.8.1 Закончил 1.8.1

--- gavrik/static/shop/js/scripts.js
+++ gavrik/static/shop/js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+
     var form = $('#form-buying-product');
     console.log(form);
+
     form.on('submit', function(e){
         e.preventDefault();
         console.log('Test');
@@ -13,11 +15,31 @@ $(document).ready(function(){
         console.log(product_id);
         console.log(product_name);
         console.log(product_price);
+
+        $('.basket-items ul').append('<li>' + product_name + ' ' +nmb+ ' шт. ' + product_price + '<a class="delete-item" href=""> X </a>'+ '</li>' );
     })
-//
-//    $('.basket-container').on('click', 'hover', function(e){
-//        e.preventDefault();
-//        $('.basket-item').removeClass('hidden')
-//    }
-//    }
+
+    function showingBasket(){
+        $('.basket-items').removeClass('hidden');
+    }
+
+    $('.basket-container').on('click', function(e) {
+        e.preventDefault();
+        showingBasket();
+    })
+
+    $('.basket-container').mouseover(function() {
+        showingBasket();
+    })
+
+
+     $('.basket-container').mouseout(function() {
+        $('.basket-items').addClass('hidden');
+    })
+
+    $(document).on('click', '.delete-item', function(e) {
+        e.preventDefault();
+        $(this).closest('li').remove();
+    })
+
 });
