4430490e341efbeb0b4f318e4033b6a63ce840a4
ComeOnGavrik <mihailgavrik03@gmail.com>
2024-12-03 20:11:14 +0300

    Добавлена страница ТЗ и резуме
    добавлены стили

--- gavrik/main/urls.py
+++ gavrik/main/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
+    path('taska', views.showing_reference_page, name='reference')
 ]
--- gavrik/main/views.py
+++ gavrik/main/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.views.generic import CreateView
 
+
 # Create your views here.
 def index(request):
-
     data = {
         'tittle': 'Главная страница!',
         'values': ['a', 'b', 'sfsfd'],
@@ -17,7 +17,12 @@ def index(request):
 
     return render(request, 'main/index.html', data)
 
+
 def about(request):
     return render(request, 'main/about.html')
 
-#Регистрация пользователей
+
+# Регистрация пользователей
+
+def showing_reference_page(request):
+    return render(request, 'main/terms_of_reference.html')
