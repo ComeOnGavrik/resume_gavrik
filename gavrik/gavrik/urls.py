c34152e479159cf24da7eb61e3d953cd27e5a8ff
ComeOnGavrik <mihailgavrik03@gmail.com>
2024-12-04 13:19:48 +0300

    Небольшая коспетика для начала разарботки норм фронт-енда

--- gavrik/gavrik/urls.py
+++ gavrik/gavrik/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('main.urls')),
     path('news/', include('news.urls')),
     path('users/', include('users.urls', namespace='users')),
